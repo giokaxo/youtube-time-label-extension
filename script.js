2057192ed40cfd96d4c69a9d9ba1abc24388e192
Giorgi Kakhoshvili <giorgi.kakhoshvili@rebilly.com>
2024-03-27 21:02:57 +0400

    fix initial state

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const STORAGE_KEY = 'HIDE_YOUTUBE_TIME_LABEL';
+const STORAGE_KEY = 'HIDE_YOUTUBE_TIME_LABELs';
 const stylesheet = document.createElement('style');
 stylesheet.id = STORAGE_KEY;
 const rule = `ytd-thumbnail-overlay-time-status-renderer {display: none;}`;
 stylesheet.innerHTML = rule;
 let enabled = true;
+
+window.addEventListener('load', onload);
+
+async function onload() {
+  const value = await getValue();
+  await setValue(value);
+  toggleStylesheet(value);
+}
 
 chrome.storage.onChanged.addListener(async (changes) => {
   for (let [key, { newValue }] of Object.entries(changes)) {
@@ -14,18 +22,15 @@ chrome.storage.onChanged.addListener(async (changes) => {
   }
 });
 
-window.addEventListener('load', async () => {
-  enabled = await getValue();
-  toggleStylesheet(enabled);
-});
 
-async function getValue() {
+async function getValue(defaultValue = true) {
   const result = await chrome.storage.local.get([STORAGE_KEY]);
-  return result[STORAGE_KEY];
+  return result[STORAGE_KEY] || defaultValue;
 }
 
-function setValue(val) {
-  enabled = val;
+async function setValue(value) {
+  enabled = value;
+  return chrome.storage.local.set({ [STORAGE_KEY]: value })
 }
 
 function insertStyleSheet() {
